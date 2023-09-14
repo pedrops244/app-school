3519b7cc44449360021be3fb4612485dd42fdb13
Pedro Souza <pedrops244@hotmail.com>
2023-09-14 19:44:43 -0300

    using history

--- src/pages/Page404/index.js
+++ src/pages/Page404/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
+import history from '../../services/history';
 
 export default function Page404() {
   return (
