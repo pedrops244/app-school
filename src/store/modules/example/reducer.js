74aff8f6eea3316985d3a71ce570ba5b29325190
Pedro Souza <pedrops244@hotmail.com>
2023-09-16 13:39:48 -0300

    create, use and config redux-saga

--- src/store/modules/example/reducer.js
+++ src/store/modules/example/reducer.js
@@ -1,13 +1,24 @@
+import * as types from '../types';
+
 const initialState = {
   botaoClicado: false,
 };
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case 'BOTAO_CLICADO': {
+    case types.BOTAO_CLICADO_SUCESS: {
+      console.log('Sucesso =)');
       const newState = { ...state };
       newState.botaoClicado = !newState.botaoClicado;
       return newState;
+    }
+    case types.BOTAO_CLICADO_FAILURE: {
+      console.log('Deu error =(');
+      return state;
+    }
+    case types.BOTAO_CLICADO_REQUEST: {
+      console.log('Estou fazendo a requisição');
+      return state;
     }
 
     default: {
